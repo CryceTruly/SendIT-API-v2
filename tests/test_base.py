6d93d4c5259ca558ffd1ba7864b57c2aca00d553
crycetruly <crycetruly@gmail.com>
2018-11-18 21:40:46 +0300

    wrote tests for /auth routes

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,82 +1,88 @@
-import json
 import unittest
+import json
 
-from app import app
-from app.models import Parcel
+from app import app, helper
+from app.database.database import Database as db
+from app.helper import Helper
 
 
-class TestsStart(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
+
+    def create_app(self):
+        """
+        Create an instance of a class with testing
+        """
+        return app
 
     def setUp(self):
-        self.app = app.test_client()
+        self.client = app.test_client(self)
+        with app.app_context():
+            connect = db()
+            connect.drop_tables()
+            connect.create_tables()
 
-    def test_if_can_get_users(self):
-        response = self.app.get('api/v1/users')
-        data = json.loads(response.data.decode())
-        self.assertIn('count', data)
+    def tearDown(self):
+        with app.app_context():
+            connect = db()
+            connect.drop_tables()
+            connect.create_tables()
 
-
-    def test_if_can_get_parcels(self):
-        response = self.app.get('api/v1/parcels')
-        data = json.loads(response.data.decode())
-        self.assertIn('count', data)
-
-    def test_parcel_request_not_json(self):
-        """ Test order content to be posted not in json format """
-        expectedreq = {
-            'pickup_address': 'Kampala Kikoni Makerere 13',
-            'destination_address': 'Mabarara Kikoni Home 13',
-            'comment_description': 'My parcels contain a laptop,please deliver',
-            'status': 'In Transit',
-            'current_location': 'Mabarara Kikoni Home 13',
-            'created': "Sat, 10 Nov 2018 13:46:41 GMT",
-            'recipient_address': 'Julie Muli',
-            'recipient_phone': '0767876666',
-            'recipient_email': 'recipient@email.com'
-
+    def signup_user(self, username, email, telephone_number, password, is_admin):
+        """
+        Method to define user registration details
+        """
+        register = {
+            "username": username,
+            "email": email,
+            "phone_number": telephone_number,
+            "password": password,
+            "is_admin": is_admin
         }
-        result = self.app.post(
-            '/api/v1/parcels',
-            content_type='text/html',
-            data=json.dumps(expectedreq)
+        return self.client.post(
+            '/api/v1/auth/signup',
+            content_type="application/json",
+            data=json.dumps(register)
         )
-        self.assertEqual(result.status_code, 415)
-        self.assertIn('Content-type must be application/json', str(result.data))
 
-    def test_create_user_request_not_json(self):
-        """ Test order content to be posted not in json format """
-        expectedreq = {
-            'pickup_address': 'Kampala Kikoni Makerere 13',
-            'destination_address': 'Mabarara Kikoni Home 13',
-            'comment_description': 'My parcels contain a laptop,please deliver',
-            'status': 'In Transit',
-            'current_location': 'Mabarara Kikoni Home 13',
-            'created': "Sat, 10 Nov 2018 13:46:41 GMT",
-            'recipient_address': 'Julie Muli',
-            'recipient_phone': '0767876666',
-            'recipient_email': 'recipient@email.com'
+    def login_user(self, username, password):
+        """
+        Method to define user login details
+        """
+        login = {
+            "username": username,
+            "password": password
         }
-        result = self.app.post(
-            '/api/v1/users',
-            content_type='text/html',
-            data=json.dumps(expectedreq)
+        return self.client.post(
+            '/api/v1/auth/login',
+            content_type="application/json",
+            data=json.dumps(login)
         )
-        self.assertEqual(result.status_code, 401)
-        self.assertIn('Content-type must be application/json', str(result.data))
 
-    def test_parcel(self):
-        parcel = Parcel(1, 'startadd', 'endadd', 'myc comment', 'delivered', 'dest addree', 'now', 1, 'recipt add',
-                        '075633333333'
-                        , 'testuser@gmail.com')
-        self.assertIn("id:1",str(parcel))
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def post_parcel(self, destination_address, pickup_address, comment_description, user_id, sender_email,
+                    recipient_phone, recipient_email, recipient_name, weight, status, latlng, destlatlng,
+                 token):
+        """
+        Define post attributes and route
+        """
+        helper = Helper()
+        data = {
+            "user_id": user_id,
+            "pickup_address": pickup_address,
+            "destination_address": destination_address,
+            "comment_description": comment_description,
+            "status": status,
+            "current_location": pickup_address,
+            "sender_email": sender_email,
+            "recipient_phone": recipient_phone,
+            "recipient_email": recipient_email,
+            "recipient_name": recipient_name,
+            "weight": weight,
+            "distance": helper.get_distance(latlng, destlatlng),
+            "price": helper.get_charge(weight, helper.get_distance(latlng, destlatlng))
+        }
+        return self.client.post(
+            '/api/v2/parcels',
+            headers=dict(Authorization='Bearer' " " + token),
+            content_type="application/json",
+            data=json.dumps(data)
+        )
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,187 +1,224 @@
+from app.database.database import Database
+from app.model.models import User
+from tests.test_base import BaseTestCase
 import json
-import unittest
 
-from app import app
+db = Database()
 
 
-class TestsUsers(unittest.TestCase):
-    def setUp(self):
-        self.client = app.test_client(self)
+class TestAuth(BaseTestCase):
 
-    def test_create_user(self):
+    def test_user_class(self):
+        user = User(1, "Cryce Truly", "crycetruly","crycetruly@gmail.com", "0756778877", 'password')
+        self.assertTrue(user)
+
+    def test_details_json_format(self):
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "crycetruly","crycetruly@gmail.com", "0756778877", 'password')
+            self.assertTrue(result.content_type == "application/json")
+
+    def test_email_not_valid(self):
         """
-        checks if a user can be created
-        """
-        expecteduser_obj = {
-            "fullname": "John terry",
-            "username": "jdoe",
-            "phone_number": "0756544544",
-            "email": "email000000@test.com",
-            "password": "passwod="
-
+         Test for invalid email address
+         """
+        register = {
+            "username": "crycetruly",
+            "email": "email",
+            "phone_number": "0756787865",
+            "password": "password",
+            "is_admin": False
         }
-        response = self.client.post(
-            "api/v1/users",
-            data=json.dumps(expecteduser_obj),
-            content_type="application/json")
-        data = json.loads(response.data.decode())
+        rs = self.client.post(
+            '/api/v2/auth/signup',
+            content_type="application/json",
+            data=json.dumps(register)
+        )
+        data = json.loads(rs.data.decode())
+        self.assertEqual(rs.status_code, 202)
+        self.assertTrue(data['status'] == 'Error')
+        self.assertTrue(data['message'] == 'Missing or wrong email format')
 
-        self.assertEqual(
-            data['msg'], "user created")
-        self.assertEqual(response.status_code, 201)
-
-    def test_cannot_create_a_user_without_email(self):
+    def test_short_password(self):
         """
-        checks if cannot create a user who has no email
+        Test for short password
         """
-        users = []
-        expecteduser_obj = {
-            "fullname": "John terry",
-            "username": "je",
-            "phone_number": "0756544544",
-            "password": "passwod="
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "crycetruly", "crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 202)
+            data = json.loads(result.data.decode())
+            self.assertTrue(data['status'] == 'Failed')
+            self.assertTrue(data['message'] == 'Ensure password is morethan 4 characters')
 
-        }
-        users.append(expecteduser_obj)
-        user_obj = {
-            "fullname": "John terry",
-            "username": "je",
-            "phone_number": "0756544544",
-            "password": "passwod="
-
-        }
-        response = self.client.post(
-            "api/v1/users",
-            data=json.dumps(user_obj),
-            content_type="application/json")
-        data = json.loads(response.data.decode())
-        self.assertEqual(data['msg'], 'Bad request')
-        self.assertEqual(data['success'], False)
-        self.assertEqual(response.status_code, 400)
-
-    def test_user_cannot_have_someones_user_name(self):
-        users = []
-        expecteduser_obj = {
-            "fullname": "John terry",
-            "username": "jdoe",
-            "phone_number": "0756544544",
-            "email": "email@test.com",
-            "password": "passwod="
-
-        }
-        users.append(expecteduser_obj)
-        user_obj = {
-            "fullname": "John terry",
-            "username": "jdoe",
-            "phone_number": "0756544544",
-            "email": "fred@test.com",
-            "password": "yestpass"
-
-        }
-        response = self.client.post(
-            "api/v1/users",
-            data=json.dumps(user_obj),
-            content_type="application/json")
-        data2 = json.loads(response.data.decode())
-        self.assertEqual(data2['msg'], 'Username is already taken')
-        self.assertEqual(data2['success'], False)
-        self.assertEqual(response.status_code, 409)
-
-    def test_user_cannot_have_someone_else_email_address(self):
-        usersList = []
-        expecteduser_obj = {
-            "fullname": "John terry",
-            "username": "fredy",
-            "phone_number": "0756544544",
-            "email": "fredy@test.com",
-            "password": "passwod="
-
-        }
-        usersList.append(expecteduser_obj)
-        user_obj2 = {
-            "fullname": "John terry",
-            "username": "jdoe",
-            "phone_number": "0756544544",
-            "email": "fredy@test.com",
-            "password": "yestpass"
-
-        }
-        response = self.client.post(
-            "api/v1/users",
-            data=json.dumps(user_obj2),
-            content_type="application/json")
-        data2 = json.loads(response.data.decode())
-        self.assertEqual(data2['msg'], 'Username is already taken')
-        self.assertEqual(data2['success'], False)
-        self.assertEqual(response.status_code, 409)
-
-    def test_user_cannot_have_an_invalid_email(self):
+    def test_username_is_string(self):
         """
-        tests if user supplies a valid email
+        Test username isstring
         """
-        user_obj = {
-            "fullname": "John terry",
-            "username": "jdoe",
-            "phone_number": "0756544544",
-            "email": "est.com",
-            "password": "passwod="
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "355", "crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 202)
+            data = json.loads(result.data.decode())
+            self.assertTrue(data['status'] == 'Type Error')
+            self.assertTrue(data['message'] == 'username must all be string')
 
+    def test_spaces_in_username(self):
+        """
+        Test username has no spaces between characters
+        """
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "cryce truly","crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 400)
+            data = json.loads(result.data.decode())
+            self.assertTrue(data['status'] == 'Space Error')
+            self.assertTrue(data['message'] == 'Username should not have space, better user _')
+
+    def test_username_not_provided(self):
+        """
+        Test username field left empty
+        """
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "","crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 400)
+            data = json.loads(result.data.decode())
+            self.assertTrue(data['status'] == 'Missing')
+            self.assertTrue(data['message'] == 'Username required')
+
+    def test_user_dat_not_json(self):
+        """
+        Test Content_type not application/json for sign up request
+        """
+        rv = self.client.post(
+            '/api/v1/auth/signup',
+            content_type="text",
+            data=json.dumps(dict({'status': 'register'}))
+        )
+        self.assertEqual(rv.status_code, 202)
+        self.assertIn(
+            '"message": "Content-type must be in json"', str(rv.data))
+
+    def test_content_type_4_login_not_json(self):
+        """
+        Test Content_type not application/json for login request
+        """
+        rv = self.client.post(
+            '/api/v1/auth/login',
+            content_type="text",
+            data=json.dumps(dict({'status': 'register'}))
+        )
+        self.assertEqual(rv.status_code, 202)
+        self.assertIn('"message": "Content-type must be in json"', str(rv.data))
+
+    def test_user_already_exist(self):
+        with self.client:
+            self.signup_user(
+                "Cryce Truly", "crycetruly","crycetruly@gmail.com", "0756778877", 'password')
+            result = self.signup_user(
+                "Cryce Truly", "crycetruly", "crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 409)
+            data = json.loads(result.data.decode())
+            self.assertTrue(data['status'] == 'Failed')
+
+    def test_successful_login(self):
+        with self.client:
+            self.signup_user(
+                "Cryce Truly", "crycetruly", "crycetruly@gmail.com", "0756778877", 'password')
+            response = self.login_user("crycetruly", "password")
+            res = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(res['username'] == 'crycetruly')
+            self.assertEqual(
+                "You have succesfully logged in.", str(res['message']))
+            self.assertTrue(res['auth_token'])
+
+    def test_login_credentials(self):
+        with self.client:
+            self.signup_user(
+                 "Cryce Truly", "crycetruly", "crycetruly@gmail.com", "0756778877", 'password')
+            resp = self.login_user("crycetruly", "wrongpassw")
+            res = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 401)
+            self.assertTrue(res['status'] == 'Failed')
+            self.assertEqual(
+                'incorrect password', str(res['message']))
+
+    def test_successful_signup(self):
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "crycetruly", "crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 201)
+            res = json.loads(result.data.decode())
+            self.assertTrue(res['status'] == 'Success')
+            self.assertEqual(
+                'User account successfully created, log in',
+                str(res['message']))
+
+    def test_invalid_token(self):
+        res = self.client.get(
+            '/api/v2/parcels/',
+            headers=dict(Authorization='Bearer ywjjkjkjkwe'))
+        data = json.loads(res.data.decode())
+        self.assertIn("Invalid token. Please log in again.", str(data))
+        self.assertEqual(res.status_code, 401)
+
+    def test_missing_username_keyword(self):
+        register = {
+            "email": "email@gmail.com",
+            "location": "location",
+            "password": "password",
+            "is_admin": True
         }
-        response = self.client.post(
-            "api/v1/users",
-            data=json.dumps(user_obj),
-            content_type="application/json")
-        data = json.loads(response.data.decode())
-        self.assertEqual(data['msg'], 'Email is badly formatted')
-        self.assertEqual(data['success'], False)
-        self.assertEqual(response.status_code, 400)
+        rs = self.client.post(
+            '/api/v2/auth/signup',
+            content_type="application/json",
+            data=json.dumps(register)
+        )
+        data = json.loads(rs.data.decode())
+        self.assertEqual(data['KeyError'], "'username'")
 
-    def test_cannot_get_user_parcels(self):
-        """
-        checks if a user that doesnt exist can not get parcel request orders
-        """
-        response = self.client.get(
-            "api/v1/users/5999/parcels",
-            content_type="application/json")
-        # we should get an ok
-        self.assertEqual(response.status_code, 404)
-
-    def test_get_user_parcels(self):
-        '''
-        creates a user then attempts to get their items
-        :param self:
-        :return:
-        '''
-        expecteduser_obj = {
-            "fullname": "John terry",
-            "username": "jdoe",
-            "phone_number": "0756544544",
-            "email": "email000000@test.com",
-            "password": "passwod="
-
+    def test_password_keyword_missing(self):
+        login = {
+            "username": "my name"
         }
-        self.client.post(
-            "api/v1/users",
-            data=json.dumps(expecteduser_obj),
-            content_type="application/json")
-        """
-        checks if a user that doesnt exist can not get parcel request orders
-        """
-        response = self.client.get(
-            "api/v1/users/1/parcels",
-            content_type="application/json")
-        # we should get an ok
-        self.assertEqual(response.status_code, 200)
+        rv = self.client.post(
+            '/api/v2/auth/login',
+            content_type="application/json",
+            data=json.dumps(login)
+        )
+        data = json.loads(rv.data.decode())
+        self.assertEqual(data['KeyError'], "'password'")
 
-    def test_get_a_no_users_message(self):
-        '''
-        tests if a user gets a readable no users message when users are not there
-        :return:
-        '''
-        response = self.client.get('api/v1/users', content_type='application/json')
-        data = json.loads(response.data.decode())
-        count = data['count']
-        if count == 0:
-            self.assertEqual(data['msg'], 'No users yet')
-        self.assertEqual(response.status, '200 OK')
-   
+    def test_all_users_details(self):
+        with self.client:
+            result = self.signup_user(
+                "Cryce Truly", "crycetruly", "crycetruly@gmail.com", "0756778877", 'password')
+            self.assertEqual(result.status_code, 201)
+            res = json.loads(result.data.decode())
+            self.assertTrue(res['status'] == 'Success')
+            self.assertEqual(
+                'User account successfully created, log in',
+                str(res['message']))
+
+            users = Database().get_users()
+            user_dict = {
+                "user_id": 1,
+                "username": "crycetruly",
+                "email": "crycetruly@gmail.com",
+                "phone_number": "0756778877",
+                "is_admin": False
+            }
+            rs = self.client.get(
+                '/api/v2/users',
+                content_type="application/json",
+                data=json.dumps(user_dict)
+            )
+            data = json.loads(rs.data.decode())
+            self.assertEqual(rs.status_code, 200)
+            self.assertIn("'phone_number': '0756778877'", str(data['users']))
+
+
+
+
